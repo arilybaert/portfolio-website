b5865b9ef56f3124a2f1b3bbcf6f7292b0ce2285
arilybaert <arilybae@arteveldehs.be>
2020-08-06 14:59:52 +0200

    started on homepage

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Porfolio Ari</title>
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+</head>
+<body>
+    <!-- sidebar -->
+
+    <div class="o-sidebar">
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+
+    <!-- homepage -->
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+
+            </div>
+            <div class="col-6">
+                <p>Hi,<br/> I'm <span class="o-text-accent">Ari Lybaert</span></p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
