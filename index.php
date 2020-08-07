722dfeeca1f56cdba30dfe3e1729051d79a80779
arilybaert <arilybae@arteveldehs.be>
2020-08-07 19:01:12 +0200

    started on skills page

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Porfolio Ari</title>
-    <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
     <!-- sidebar -->
@@ -19,9 +19,9 @@
         </ul>
     </div>
 
-    <!-- homepage -->
     <div class="o-wrapper">
         <div class="container">
+            <!-- homepage -->
             <div class="row o-home">
                 <div class="col-6 m-home-left">
                     <div class="m-profilePhoto">
@@ -33,6 +33,54 @@
                     <p class="a-mainDescr">A last year developer student at the Artevelde College in Ghent </p>
                 </div>
             </div>
+
+            <!-- skills -->
+            <div class="row o-skills">
+                <div class="col-3 o-grafic">
+                    <div class="row">
+                        <div class="col-12">
+                            <div>Graphic Skills</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                        <div>Ps</div>
+                        <div>Ai</div>
+                        <div>Id</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 o-languages">
+                    <div class="row">
+                        <div class="col-12">
+                            <div>Languages</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div>NL</div>
+                            <div>Fr</div>
+                            <div>EN</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 o-technical">
+                    <div class="row">
+                        <div>Technical Skills</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div>Javascript</div>
+                            <div>React.JS</div>
+                            <div>Python</div>
+                            <div>PHP</div>
+                            <div>Laravel</div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 </body>
